ebaeb41a2932400fc1bcd0b0bb00c0a2cc6d94ca
shir-s <shir.seroussi@mail.huji.ac.il>
2025-01-27 01:26:30 +0200

    Adding ScorePopUp

--- Assets/Scripting/Enemies/EnemyHealth.cs
+++ Assets/Scripting/Enemies/EnemyHealth.cs
@@ -5,7 +5,11 @@ public class EnemyHealth : MonoBehaviour
     [SerializeField] private int maxHealth = 1;
     [SerializeField] private int currentHealth;
     private Animator animator;
-
+    
+    [Header("Score Popup")]
+    [SerializeField] private GameObject scorePopupPrefab;
+    [SerializeField] private int scoreValue = 100;
+    
     private void Awake()
     {
         animator = GetComponent<Animator>();
@@ -32,11 +36,21 @@ public class EnemyHealth : MonoBehaviour
     {
         GameManager.Instance.AddScore(100);
         animator.SetTrigger("IsDead");
+        CreateScorePopup();
         Invoke(nameof(OnDeathSequenceEnd), 1f);
     }
 
     private void OnDeathSequenceEnd()
     {
         gameObject.SetActive(false);
+    }
+    
+    private void CreateScorePopup()
+    {
+        if (scorePopupPrefab != null)
+        {
+            // יצירת ה-ScorePopup במיקום של האויב
+            Instantiate(scorePopupPrefab, transform.position, Quaternion.identity);
+        }
     }
 }
