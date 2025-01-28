6505f2cd031069ba7b896bda2e7fe09ae0349c19
shir-s <shir.seroussi@mail.huji.ac.il>
2025-01-28 22:33:02 +0200

    Updated input to load the scene while pressing Enter.

--- Assets/Scripting/General/GameManager.cs
+++ Assets/Scripting/General/GameManager.cs
@@ -9,23 +9,22 @@ public class GameManager : MonoSingleton<GameManager>
     public string stageSceneName = "StageScene"; // שם הסצנה שמופיעה לפני תחילת סבב חדש
     public string gameOverSceneName = "GameOverScene";
     public string gameSceneName = "GameScene";
+
     private bool isTimerRunning = false;
-    
     private bool isTransitioning = false;
 
     private void Awake()
     {
         DontDestroyOnLoad(gameObject); // הבטחה שהאובייקט לא יושמד בעת טעינת סצנה חדשה
     }
-    
+
     private void Start()
     {
-        if (livesRemaining == 0) livesRemaining = 3;
+        if (livesRemaining == 0) livesRemaining = 3; // איפוס כמות החיים בתחילת המשחק
         UpdateUI();
         StartTimer();
-        //InvokeRepeating(nameof(UpdateTimer), 1f, 1f);
     }
-    
+
     private void UpdateUI()
     {
         UIManager.Instance.UpdateScoreText(score);
@@ -37,22 +36,22 @@ public class GameManager : MonoSingleton<GameManager>
     {
         if (isTimerRunning) return; // אם הטיימר כבר פועל, לא להפעיל שוב
         CancelInvoke(nameof(UpdateTimer)); // איפוס כל הטיימרים
+        isTimerRunning = true; // דגל להתחלת טיימר
         InvokeRepeating(nameof(UpdateTimer), 1f, 1f); // התחלת טיימר
-        isTimerRunning = true;
     }
-    
+
     private void StopTimer()
     {
         CancelInvoke(nameof(UpdateTimer)); // עצירת כל הטיימרים לפונקציה UpdateTimer
         isTimerRunning = false; // עדכון הדגל
     }
-    
+
     private void UpdateTimer()
     {
         if (timeRemaining > 0)
         {
             timeRemaining--;
-            UIManager.Instance.UpdateTimeText(timeRemaining);
+            UIManager.Instance.UpdateTimeText(timeRemaining); // עדכון ה-UI
 
             if (timeRemaining <= 0)
             {
@@ -66,7 +65,7 @@ public class GameManager : MonoSingleton<GameManager>
         score += points;
         UIManager.Instance.UpdateScoreText(score);
     }
-    
+
     public void LoseLife()
     {
         Debug.Log("LoseLife called!");
@@ -74,6 +73,8 @@ public class GameManager : MonoSingleton<GameManager>
         isTransitioning = true;
         livesRemaining--;
         UIManager.Instance.UpdateLivesText(livesRemaining);
+        StopTimer(); // עצירת הטיימר הנוכחי
+
         if (livesRemaining > 0)
         {
             TransitionToStageScene();
@@ -83,7 +84,7 @@ public class GameManager : MonoSingleton<GameManager>
             EndGame();
         }
     }
-    
+
     private void TransitionToStageScene()
     {
         Debug.Log("Transitioning to StageScene...");
@@ -96,35 +97,165 @@ public class GameManager : MonoSingleton<GameManager>
         yield return new WaitForSeconds(2f); // המתנה ל-2 שניות
         RestartGame();
     }
-    
+
     private void RestartGame()
     {
         Debug.Log("Restarting Game...");
-        StopTimer(); // עצירת טיימר ישן לפני הטעינה
-        SceneManager.LoadScene(gameSceneName);
-        isTransitioning = false;
+        SceneManager.LoadScene(gameSceneName); // טעינת סצנת המשחק
+        isTransitioning = false; // איפוס דגל המעבר
+
+        // בדיקה שה-UIManager מתעדכן מחדש
         StartCoroutine(WaitForSceneLoad());
     }
 
     private System.Collections.IEnumerator WaitForSceneLoad()
     {
-        // מחכה לסיום טעינת הסצנה
+        // מחכה לטעינת הסצנה
         yield return new WaitUntil(() => SceneManager.GetActiveScene().name == gameSceneName);
 
-        Debug.Log("GameScene loaded. Updating UI...");
-        UIManager.Instance.AssignUIComponents(); // משייכים מחדש את רכיבי ה-UI
-        UIManager.Instance.UpdateTimeText(timeRemaining);
-        UIManager.Instance.UpdateLivesText(livesRemaining);
-        UIManager.Instance.UpdateScoreText(score);
-
-        // הפעלת הטיימר מחדש
-        StartTimer();
+        Debug.Log("GameScene loaded. Initializing game state...");
+        timeRemaining = 200; // איפוס הזמן
+        UIManager.Instance.AssignUIComponents(); // משייך מחדש את רכיבי ה-UI
+        UpdateUI(); // עדכון ה-UI עם הנתונים הנוכחיים
+        StartTimer(); // הפעלת הטיימר מחדש
     }
 
     private void EndGame()
     {
         Debug.Log("Game Over!");
-        CancelInvoke(nameof(UpdateTimer)); // עצירת הטיימר
+        StopTimer(); // עצירת הטיימר
         SceneManager.LoadScene(gameOverSceneName); // טעינת סצנת הפסד
     }
 }
+
+
+
+
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
+//
+// public class GameManager : MonoSingleton<GameManager>
+// {
+//     public int score = 0;
+//     public int timeRemaining = 200;
+//     public int livesRemaining = 3;
+//     public string stageSceneName = "StageScene";
+//     public string gameOverSceneName = "GameOverScene";
+//     public string gameSceneName = "GameScene";
+//     private bool isTimerRunning = false;
+//     
+//     private bool isTransitioning = false;
+//
+//     private void Awake()
+//     {
+//         DontDestroyOnLoad(gameObject);
+//     }
+//     
+//     private void Start()
+//     {
+//         if (livesRemaining == 0) livesRemaining = 3;
+//         UpdateUI();
+//         StartTimer();
+//     }
+//     
+//     private void UpdateUI()
+//     {
+//         UIManager.Instance.UpdateScoreText(score);
+//         UIManager.Instance.UpdateTimeText(timeRemaining);
+//         UIManager.Instance.UpdateLivesText(livesRemaining);
+//     }
+//
+//     private void StartTimer()
+//     {
+//         if (isTimerRunning) return; 
+//         CancelInvoke(nameof(UpdateTimer));
+//         InvokeRepeating(nameof(UpdateTimer), 1f, 1f);
+//         isTimerRunning = true;
+//     }
+//     
+//     private void StopTimer()
+//     {
+//         CancelInvoke(nameof(UpdateTimer));
+//         isTimerRunning = false;
+//     }
+//     
+//     private void UpdateTimer()
+//     {
+//         if (timeRemaining > 0)
+//         {
+//             timeRemaining--;
+//             UIManager.Instance.UpdateTimeText(timeRemaining);
+//
+//             if (timeRemaining <= 0)
+//             {
+//                 LoseLife();
+//             }
+//         }
+//     }
+//
+//     public void AddScore(int points)
+//     {
+//         score += points;
+//         UIManager.Instance.UpdateScoreText(score);
+//     }
+//     
+//     public void LoseLife()
+//     {
+//         Debug.Log("LoseLife called!");
+//         if (isTransitioning) return;
+//         isTransitioning = true;
+//         livesRemaining--;
+//         UIManager.Instance.UpdateLivesText(livesRemaining);
+//         if (livesRemaining > 0)
+//         {
+//             TransitionToStageScene();
+//         }
+//         else
+//         {
+//             EndGame();
+//         }
+//     }
+//     
+//     private void TransitionToStageScene()
+//     {
+//         Debug.Log("Transitioning to StageScene...");
+//         SceneManager.LoadScene(stageSceneName);
+//         StartCoroutine(WaitBeforeTransitionToStageScene());
+//     }
+//
+//     private System.Collections.IEnumerator WaitBeforeTransitionToStageScene()
+//     {
+//         yield return new WaitForSeconds(2f);
+//         RestartGame();
+//     }
+//     
+//     
+//     
+//     private void RestartGame()
+//     {
+//         Debug.Log("Restarting Game...");
+//         SceneManager.LoadScene(gameSceneName);
+//         isTransitioning = false;
+//         StartCoroutine(WaitForSceneLoad());
+//     }
+//
+//     private System.Collections.IEnumerator WaitForSceneLoad()
+//     {
+//         yield return new WaitUntil(() => SceneManager.GetActiveScene().name == gameSceneName);
+//
+//         Debug.Log("GameScene loaded. Updating UI...");
+//         UIManager.Instance.AssignUIComponents();
+//         UIManager.Instance.UpdateTimeText(timeRemaining);
+//         UIManager.Instance.UpdateLivesText(livesRemaining);
+//         UIManager.Instance.UpdateScoreText(score);
+//
+//         StartTimer();
+//     }
+//
+//     private void EndGame()
+//     {
+//         Debug.Log("Game Over!");
+//         CancelInvoke(nameof(UpdateTimer));
+//         SceneManager.LoadScene(gameOverSceneName);
+//     }
+// }
--- Assets/Scripting/SceneController.cs
+++ Assets/Scripting/SceneController.cs
@@ -7,7 +7,7 @@ public class SceneController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Return))
         {
           SceneManager.LoadScene(sceneIndex);
         }
